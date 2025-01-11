b2c47832f8f3c6025bd12602a8672f7dd7b17255
Kabir <ibtikabir319@gmail.com>
2025-01-12 02:37:00 +0600

    seconcd commit

---
+++ index.js
@@ -0,0 +1,125 @@
+const express = require('express');
+const app = express()
+const bodyParser = require('body-Parser');
+const mysql = require('mysql');
+const port = process.env.PORT || 3001;
+const cors = require('cors');
+
+
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.listen(3001, () => {
+
+    console.log('Server is running on Port 3001')
+
+})
+
+
+const db = mysql.createPool({
+
+    host: "localhost",
+
+    user: "root",
+
+    password: "",
+
+    database: "maestro_project"
+
+})
+
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    const body = req.body
+    const sql = "INSERT INTO employees (name, username, password, designation_id, department, create_date,update_date) VALUES(?,?,?,?,?,?,?)"
+
+    db.query(sql, [body.name, body.username, body.password, body.designation, body.dept, body.createDate, body.updateDate], (err, result) => {
+
+        console.log(err);
+
+    })
+})
+
+app.post('/saveDesig', (req, res) => {
+    console.log(req.body);
+    const body = req.body
+    const sql = "INSERT INTO designation (title) VALUES(?)"
+
+    db.query(sql, [body.dTitle], (err, result) => {
+
+
+        console.log(err);
+        res.send(result)
+
+    })
+})
+app.post('/addDept', (req, res) => {
+    console.log(req.body);
+    const body = req.body
+    const sql = "INSERT INTO department (title) VALUES(?)"
+
+    db.query(sql, [body.deptTitle], (err, result) => {
+
+
+        console.log(err);
+        res.send(result)
+
+    })
+})
+app.delete('/designations/:id',(req,res)=>{
+
+    const id = req.params.id;
+    console.log('please Delete id from database : ', id)
+    const sql="DELETE FROM designation WHERE designation_id=?"
+    
+    db.query(sql , [id] ,(err,result)=>{
+    
+    if(err) console.log(err);
+    
+    res.send(result);
+    
+    })
+    
+    })
+
+
+app.get('/designations', (req, res) => {
+
+    const sql = "SELECT * FROM designation"
+
+    db.query(sql, (err, result) => {
+
+        res.send(result);
+
+    })
+
+})
+
+app.get('/designations/:id', (req,res)=>{
+    const id = req.params.id;
+    const sql = "SELECT * FROM designation WHERE designation_id=?"
+    db.query(sql , [id] ,(err,result)=>{
+    
+        if(err) console.log(err);
+        
+        res.send(result);
+        
+        })
+   
+  })
+  app.put('/designations/:id',(req,res)=>{
+    const id = req.params.id;
+    console.log('Please Update this id from the database : ', id)
+    const body = req.body;
+    const sql = "UPDATE  designation SET title=? WHERE designation_id=?"
+    db.query(sql , [body.dTitle, id] ,(err,result)=>{
+
+        if(err) console.log(err);
+        
+        res.send(result);
+        
+        })
+})
---
+++ maestro_project.sql
@@ -0,0 +1,149 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 09, 2025 at 01:39 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `maestro_project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department`
+--
+
+CREATE TABLE `department` (
+  `department_id` int(11) NOT NULL,
+  `title` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `department`
+--
+
+INSERT INTO `department` (`department_id`, `title`) VALUES
+(1, 'Engineering'),
+(2, 'Management'),
+(3, 'Human Resources'),
+(4, 'Finance'),
+(5, 'Quality Assurance'),
+(6, 'Engineering');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `designation`
+--
+
+CREATE TABLE `designation` (
+  `designation_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `designation`
+--
+
+INSERT INTO `designation` (`designation_id`, `title`) VALUES
+(1, 'Software Engineer'),
+(2, 'HR'),
+(3, 'Software Engineer'),
+(4, 'HR'),
+(5, 'Accountant'),
+(6, 'Software Tester'),
+(7, 'Intern'),
+(8, 'Hello'),
+(9, 'Hello'),
+(10, 'Student');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employees`
+--
+
+CREATE TABLE `employees` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `department` varchar(255) NOT NULL,
+  `create_date` date NOT NULL,
+  `update_date` date NOT NULL,
+  `designation_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `department`
+--
+ALTER TABLE `department`
+  ADD PRIMARY KEY (`department_id`);
+
+--
+-- Indexes for table `designation`
+--
+ALTER TABLE `designation`
+  ADD PRIMARY KEY (`designation_id`);
+
+--
+-- Indexes for table `employees`
+--
+ALTER TABLE `employees`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `designation_id` (`designation_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `department`
+--
+ALTER TABLE `department`
+  MODIFY `department_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `designation`
+--
+ALTER TABLE `designation`
+  MODIFY `designation_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `employees`
+--
+ALTER TABLE `employees`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `employees`
+--
+ALTER TABLE `employees`
+  ADD CONSTRAINT `employees_ibfk_1` FOREIGN KEY (`designation_id`) REFERENCES `designation` (`designation_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
